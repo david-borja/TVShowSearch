babb79dc042ea5e09f4831c86603f6618adc3bdb
dav1dborja <davidborjamartinez@gmail.com>
2021-01-05 15:55:21 +0100

    single commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("#searchForm");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+  removeImages();
+  makeImages(res.data);
+  form.elements.query.value = "";
+});
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
+
+const removeImages = () => {
+  const images = document.querySelectorAll("img");
+  if (images.length) {
+    for (let image of images) {
+      image.remove();
+    }
+  }
+};
